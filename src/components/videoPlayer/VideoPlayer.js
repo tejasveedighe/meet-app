0b1d4f74d620e56ca35b564cb41877d311ed9f93
tejasveedighe <tejasveedighe@gmail.com>
2023-10-05 14:01:54 +0530

    Update the player with duration and seeking range

--- src/components/videoPlayer/VideoPlayer.js
+++ src/components/videoPlayer/VideoPlayer.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { IconContext } from "react-icons";
 import { BiPause, BiPlay, BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 import { url } from "../helper";
@@ -8,6 +8,11 @@ export default function VideoPlayer() {
 	const videoRef = useRef(null);
 	const [isPlaying, setIsPlaying] = useState(false);
 
+	const [currentTime, setCurrentTime] = useState([0, 0]);
+	const [currentTimeSec, setCurrentTimeSec] = useState();
+	const [duration, setDuration] = useState([0, 0]);
+	const [durationSec, setDurationSec] = useState();
+
 	const handlePlay = useCallback(() => {
 		if (isPlaying) {
 			videoRef.current.pause();
@@ -16,6 +21,27 @@ export default function VideoPlayer() {
 			videoRef.current.play();
 			setIsPlaying(true);
 		}
+	}, [isPlaying]);
+
+	const sec2Min = (sec) => {
+		const min = Math.floor(sec / 60);
+		const secRemain = Math.floor(sec % 60);
+		return {
+			min: min,
+			sec: secRemain,
+		};
+	};
+	useEffect(() => {
+		const { min, sec } = sec2Min(videoRef.current.duration);
+		setDurationSec(videoRef.current.duration);
+		setDuration([min, sec]);
+
+		const interval = setInterval(() => {
+			const { min, sec } = sec2Min(videoRef.current.currentTime);
+			setCurrentTimeSec(videoRef.current.currentTime);
+			setCurrentTime([min, sec]);
+		}, 1000);
+		return () => clearInterval(interval);
 	}, [isPlaying]);
 
 	return (
@@ -28,27 +54,41 @@ export default function VideoPlayer() {
 					className={styles.videoPlayer}
 					src={url}
 				></video>
-				<div className={styles.controls}>
-					<IconContext.Provider value={{ color: "white", size: "2em" }}>
-						<BiSkipPrevious />
-					</IconContext.Provider>
-					{isPlaying ? (
-						<button className={styles.controlButton} onClick={handlePlay}>
-							<IconContext.Provider value={{ color: "white", size: "2em" }}>
-								<BiPause />
-							</IconContext.Provider>
-						</button>
-					) : (
-						<button className={styles.controlButton} onClick={handlePlay}>
-							<IconContext.Provider value={{ color: "white", size: "2em" }}>
-								<BiPlay />
-							</IconContext.Provider>
-						</button>
-					)}
-					<IconContext.Provider value={{ color: "white", size: "2em" }}>
-						<BiSkipNext />
-					</IconContext.Provider>
+				<div className={styles.videoInfo}>
+					<div className={styles.controls}>
+						<IconContext.Provider value={{ color: "white", size: "2em" }}>
+							<BiSkipPrevious />
+						</IconContext.Provider>
+						{isPlaying ? (
+							<button className={styles.controlButton} onClick={handlePlay}>
+								<IconContext.Provider value={{ color: "white", size: "2em" }}>
+									<BiPause />
+								</IconContext.Provider>
+							</button>
+						) : (
+							<button className={styles.controlButton} onClick={handlePlay}>
+								<IconContext.Provider value={{ color: "white", size: "2em" }}>
+									<BiPlay />
+								</IconContext.Provider>
+							</button>
+						)}
+						<IconContext.Provider value={{ color: "white", size: "2em" }}>
+							<BiSkipNext />
+						</IconContext.Provider>
+					</div>
+					<div className={styles.duration}>
+						{currentTime[0]}:{currentTime[1]} / {duration[0]}:{duration[1]}
+					</div>
 				</div>
+				<input
+					type="range"
+					min="0"
+					max={durationSec}
+					defaultValue={0}
+					value={currentTimeSec}
+					className={styles.timeline}
+					onChange={(e) => (videoRef.current.currentTime = e.target.value)}
+				/>
 			</div>
 		</div>
 	);
