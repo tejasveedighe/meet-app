61aa28ca6cad46c334792e13f8b3218d81a135b7
tejasveedighe <tejasveedighe@gmail.com>
2023-10-06 10:24:46 +0530

    Change video from ist

--- src/components/videoPlayer/VideoPlayer.js
+++ src/components/videoPlayer/VideoPlayer.js
@@ -3,8 +3,11 @@ import { IconContext } from "react-icons";
 import { BiPause, BiPlay, BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 import { url } from "../helper";
 import styles from "./VideoPlayer.module.css";
+import Videos from "./Videos.json";
 
 export default function VideoPlayer() {
+	const [currentVideo, setCurrentVideo] = useState(Videos[0]);
+
 	const videoRef = useRef(null);
 	const [isPlaying, setIsPlaying] = useState(false);
 
@@ -12,7 +15,6 @@ export default function VideoPlayer() {
 	const [currentTimeSec, setCurrentTimeSec] = useState();
 	const [duration, setDuration] = useState([0, 0]);
 	const [durationSec, setDurationSec] = useState();
-
 
 	const handlePlay = useCallback(() => {
 		if (isPlaying) {
@@ -50,51 +52,75 @@ export default function VideoPlayer() {
 		videoRef.current.currentTime = e.target.value;
 	}, []);
 
+	useEffect(() => {
+		setCurrentVideo(Videos[0]);
+	}, []);
+
 	return (
-		<div className={styles.parent}>
-			<div className={styles.playerContainer}>
-				<video
-					ref={videoRef}
-					width="70%"
-					height={"70%"}
-					className={styles.videoPlayer}
-					src={url}
-				></video>
-				<div className={styles.videoInfo}>
-					<div className={styles.controls}>
-						<IconContext.Provider value={{ color: "white", size: "2em" }}>
-							<BiSkipPrevious />
-						</IconContext.Provider>
-						{isPlaying ? (
-							<button className={styles.controlButton} onClick={handlePlay}>
-								<IconContext.Provider value={{ color: "white", size: "2em" }}>
-									<BiPause />
-								</IconContext.Provider>
-							</button>
-						) : (
-							<button className={styles.controlButton} onClick={handlePlay}>
-								<IconContext.Provider value={{ color: "white", size: "2em" }}>
-									<BiPlay />
-								</IconContext.Provider>
-							</button>
-						)}
-						<IconContext.Provider value={{ color: "white", size: "2em" }}>
-							<BiSkipNext />
-						</IconContext.Provider>
+		<div className="content-center justify-center flex-col">
+			<div className="p-4">
+				<p>Id - {currentVideo.id}</p>
+				<p>Title - {currentVideo.title}</p>
+			</div>
+			<div className={styles.parent}>
+				<div className={styles.playerContainer}>
+					<video
+						ref={videoRef}
+						width="70%"
+						height="70%"
+						className={styles.videoPlayer}
+						src={currentVideo.url}
+					></video>
+					<div className={styles.videoInfo}>
+						<div className={styles.controls}>
+							<IconContext.Provider value={{ color: "white", size: "2em" }}>
+								<BiSkipPrevious />
+							</IconContext.Provider>
+							{isPlaying ? (
+								<button className={styles.controlButton} onClick={handlePlay}>
+									<IconContext.Provider value={{ color: "white", size: "2em" }}>
+										<BiPause />
+									</IconContext.Provider>
+								</button>
+							) : (
+								<button className={styles.controlButton} onClick={handlePlay}>
+									<IconContext.Provider value={{ color: "white", size: "2em" }}>
+										<BiPlay />
+									</IconContext.Provider>
+								</button>
+							)}
+							<IconContext.Provider value={{ color: "white", size: "2em" }}>
+								<BiSkipNext />
+							</IconContext.Provider>
+						</div>
+						<div className={styles.duration}>
+							{currentTime[0]}:{currentTime[1]} / {duration[0]}:{duration[1]}
+						</div>
 					</div>
-					<div className={styles.duration}>
-						{currentTime[0]}:{currentTime[1]} / {duration[0]}:{duration[1]}
-					</div>
+					<input
+						type="range"
+						min="0"
+						max={durationSec}
+						defaultValue={0}
+						value={currentTimeSec}
+						className={styles.timeline}
+						onChange={handleChangeTime}
+					/>
 				</div>
-				<input
-					type="range"
-					min="0"
-					max={durationSec}
-					defaultValue={0}
-					value={currentTimeSec}
-					className={styles.timeline}
-					onChange={handleChangeTime}
-				/>
+				<div className={styles.videoListContainer}>
+					<ul>
+						{Videos.map((vid) => (
+							<li
+								key={vid.id}
+								className={styles.videoLink}
+								onClick={() => setCurrentVideo(vid)}
+							>
+								({vid.id})-
+								{vid.title}
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
